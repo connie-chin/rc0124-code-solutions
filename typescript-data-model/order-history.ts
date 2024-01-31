7f929b2d5b9b1dc4bba3c50bef6705ba9c3f1a1a
Connie Chin <conniec96@gmail.com>
2024-01-31 03:52:42 +0000

    Completed typescript-data-model exercise.

---
+++ typescript-data-model/order-history.ts
@@ -0,0 +1,89 @@
+interface Item {
+  type: string;
+  name: string;
+  author?: string;
+  returnWindowClosed: string;
+  price: number;
+  receivedMethod?: string;
+}
+
+interface Order {
+  placedDate: string;
+  total: number;
+  orderNum: string;
+  deliverDate: string;
+  item: Item[];
+}
+
+const orderHistory: Order[] = [
+  {
+    placedDate: 'August 4, 2020',
+    total: 34.0,
+    orderNum: '114-3941689-8772232',
+    deliverDate: 'August 8, 2020',
+    item: [
+      {
+        type: 'book',
+        name: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindowClosed: 'September 7, 2020',
+        price: 31.55,
+      },
+    ],
+  },
+
+  {
+    placedDate: 'July 19, 2020',
+    total: 44.53,
+    orderNum: '113-9984268-1280257',
+    deliverDate: 'July 20, 2020',
+    item: [
+      {
+        type: 'book',
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindowClosed: 'August 19, 2020',
+        price: 41.33,
+        receivedMethod: 'handed to recipient',
+      },
+    ],
+  },
+
+  {
+    placedDate: 'July 4, 2020',
+    total: 17.22,
+    orderNum: '114-2875557-9059409',
+    deliverDate: 'July 7, 2020',
+    item: [
+      {
+        type: 'electronics',
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII u, PC. Support Turbo and Vibration Features. No Driver and No Lag- Gamecube Adapter',
+        returnWindowClosed: 'August 5, 2020',
+        price: 15.98,
+        receivedMethod: 'handed to recipient',
+      },
+    ],
+  },
+
+  {
+    placedDate: 'July 3, 2020',
+    total: 138.93,
+    orderNum: '113-2883177-2648248',
+    deliverDate: 'July 5, 2020',
+    item: [
+      {
+        type: 'electronics',
+        name: 'GameCube Controller- Super Smash Bros. Edition (Nintendo Switch)',
+        returnWindowClosed: 'August 4, 2020',
+        price: 94.95,
+      },
+      {
+        type: 'book',
+        name: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindowClosed: 'August 4, 2020',
+        price: 33.99,
+      },
+    ],
+  },
+];
