57a2836fabd9a3bddb8c7dd2cfffd574b7e0c94f
Connie Chin <conniec96@gmail.com>
2024-02-12 22:08:56 +0000

    Added notes.

--- dom-events/buttons.ts
+++ dom-events/buttons.ts
@@ -1,17 +1,17 @@
 const $click = document.querySelector('.click-button');
 if (!$click) throw new Error('The $click query failed');
 
-function handleClick(event: any): any {
+function handleClick(event: Event): void {
   console.log('button clicked');
   console.log(event);
-  console.log(event.target);
+  console.log('event.target:', event.target); // where the action took place
 }
 $click.addEventListener('click', handleClick);
 
 const $hover = document.querySelector('.hover-button');
 if (!$hover) throw new Error('The $hover query failed');
 
-function handleMouseover(event: any): any {
+function handleMouseover(event: Event): void {
   console.log('button hovered');
   console.log(event);
   console.log(event.target);
@@ -21,7 +21,7 @@ $hover.addEventListener('mouseover', handleMouseover);
 const $doubleClick = document.querySelector('.double-click-button');
 if (!$doubleClick) throw new Error('The $doubleClick query failed');
 
-function handleDoubleClick(event: any): any {
+function handleDoubleClick(event: Event): void {
   console.log('button double-clicked');
   console.log(event);
   console.log(event.target);
