1bfc5f7c503f7684249f23b36ab9691fb2ee3308
Connie Chin <conniec96@gmail.com>
2024-03-18 19:01:18 +0000

    Completed react-jsx.

---
+++ react-jsx/my-app/src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React JSX</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Total Button Clicks: {count}
+        </button>
+        <p>
+          a syntax extension for JavaScript that lets you write HTML-like markup
+          inside a JavaScript file
+        </p>
+        <p>by converting JSX to JavaScript and using react.createElement</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
