6f42375a6fd6ab5d9e19235f6d7a8d34f7db11c1
Connie Chin <conniec96@gmail.com>
2024-03-21 05:27:03 +0000

    Completed typescript-light-bulb.

---
+++ typescript-light-bulb/main.js
@@ -0,0 +1,21 @@
+'use strict';
+let isOn = true; //carrying a boolean value, we want a toggle effect. starting true
+const $circleClick = document.querySelector('.circle');
+if (!$circleClick) throw new Error('the $circleClick query failed');
+const $container = document.querySelector('.container');
+if (!$container) throw new Error('The $container query failed');
+function cClicks() {
+  if (!$circleClick || !$container)
+    throw new Error('The $circleClick or $container query failed');
+  isOn = !isOn; //whatever the initial state was, we are going to flip it. like from true to false
+  if (isOn === false) {
+    $container.className = 'container black';
+    $circleClick.className = 'circle gray';
+    // console.log(`it's off`);
+  } else if (isOn === true) {
+    $container.className = 'container';
+    $circleClick.className = 'circle';
+    // console.log(`it's on`);
+  }
+}
+$circleClick.addEventListener('click', cClicks);
