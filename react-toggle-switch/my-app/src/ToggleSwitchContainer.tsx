9d4a410212bcc4c49a91a782c695f2d736b341f0
Connie Chin <conniec96@gmail.com>
2024-03-21 05:17:53 +0000

    Started function for toggle.

---
+++ react-toggle-switch/my-app/src/ToggleSwitchContainer.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+// export function ToggleSwitch() {
+// const [toggle, setToggle] = useState(false);
+
+// function forToggleState() {
+
+// }
+
+//   return (
+//     <div className="wrapper">
+//       <div className="slider">
+//         <div className="switch"><span>ON/OFF</span></div>
+//       </div>
+//     </div>
+//   );
+// }
