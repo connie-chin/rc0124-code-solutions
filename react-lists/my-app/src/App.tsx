18d3ce104674ad2a785ab1a99068f22057313984
Connie Chin <conniec96@gmail.com>
2024-03-22 03:14:46 +0000

    Added some reformatting.

--- react-lists/my-app/src/App.tsx
+++ react-lists/my-app/src/App.tsx
@@ -1,14 +1,15 @@
 import { PokemonList } from './PokemonListContainer';
 import './App.css';
 
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' },
+];
+
 function App() {
-  const pokedex = [
-    { number: '001', name: 'Bulbasaur' },
-    { number: '004', name: 'Charmander' },
-    { number: '007', name: 'Squirtle' },
-    { number: '025', name: 'Pikachu' },
-    { number: '039', name: 'Jigglypuff' },
-  ];
   return (
     <>
       <PokemonList pokedex={pokedex} />
