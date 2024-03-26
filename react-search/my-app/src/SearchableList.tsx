9bc769933d4bd9d71e9c8bd08dc3fa9a2af5b236
Connie Chin <conniec96@gmail.com>
2024-03-26 23:49:28 +0000

    Completed react-search.

--- react-search/my-app/src/SearchableList.tsx
+++ react-search/my-app/src/SearchableList.tsx
@@ -11,15 +11,14 @@ export function SearchableList({ quotes }: Props) {
   const filteredQuotes = quotes.filter((quote) =>
     quote.toLowerCase().includes(userInput.toLowerCase())
   );
-  // function forQuoteDisplay() {
-  //   if (filteredQuotes.length = 0) {
-  //     return 'No items match the filter';
-  //   }
-  //     else {
-  //       return filteredQuotes;
-  //     }
-  // };
-  // const quoteDisplay = forQuoteDisplay();
+  function forQuoteDisplay() {
+    if (filteredQuotes.length === 0) {
+      return ['No items match'];
+    } else {
+      return filteredQuotes;
+    }
+  }
+  const quoteDisplay = forQuoteDisplay();
 
   return (
     <>
@@ -30,10 +29,9 @@ export function SearchableList({ quotes }: Props) {
           }
           value={userInput}
         />
-        {/* this where the filtering need to happen, but also it needs to be in communication w the list of items*/}
       </div>
       <div>
-        <ListOfItems quotes={filteredQuotes} />
+        <ListOfItems quotes={quoteDisplay} />
       </div>
     </>
   );
