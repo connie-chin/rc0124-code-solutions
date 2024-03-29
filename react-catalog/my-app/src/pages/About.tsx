955f17fbe57e93e69e5d25b98f98d6be8f4eb670
Connie Chin <conniec96@gmail.com>
2024-03-29 00:06:10 +0000

    Completed react-catalog.

---
+++ react-catalog/my-app/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import './About.css';
+
+export function About() {
+  return (
+    <div className="cat-container">
+      <div className="about">
+        <div className="text">What an amazing catalog!</div>
+        <img src="https://media.istockphoto.com/id/497602815/vector/emoticon-with-dollars.jpg?s=612x612&w=0&k=20&c=h0C9oMcyEjL7ayRNuHQBLL81R8opeD2sqZlqFisKXAc="></img>
+      </div>
+    </div>
+  );
+}
