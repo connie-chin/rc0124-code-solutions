e1d3887ba69ed26a2311f7d0065266b322342020
Connie Chin <conniec96@gmail.com>
2024-04-08 22:26:24 +0000

    Added notes.

--- express-mutations/express-mutations-quiz-notes.md
+++ express-mutations/express-mutations-quiz-notes.md
@@ -5,11 +5,11 @@
 Answer the following questions in the provided markdown file before turning in this exercise:
 
 - What is the significance of an HTTP request's method?
-  containing status info about the request
+  determines the selected action to perform, signifies the expectations of what should be going on
 - What is the significance of an HTTP response's status?
-  letting you know whats happening with the request, if its successful or not
+  letting you know whats happening with the request, if its successful or not... info about what happened. w proper status codes, it will let the client know what's wrong w them. if there is something wrong w our server, they only get to know that there was an error (to prevent hacking into database)
 - What does the express.json() middleware do and when would you need it?
-  parses incoming requests with JSON payloads and is based on body-parser. returns middleware that only parses JSON and only looks at requests where the Content-Type header matches the type option
+  parses incoming requests with JSON payloads and is based on body-parser. returns middleware that only parses JSON and only looks at requests where the Content-Type header matches the type option... when it's called, it returns a function to us. use calls it on each request. if there is json info it converts to what it was before and attaches to the body property. use every time u pass json info
 
 ## Notes
 
--- express-mutations/server.ts
+++ express-mutations/server.ts
@@ -12,12 +12,36 @@ const db = new pg.Pool({
 
 const app = express();
 
-// JSON middleware?
+// JSON middleware? use anytime expecting to get any request w JSON in its body. any JSON in the body, will be attached to the body property of the express object
 app.use(express.json());
 
-app.post('/api/actors', async (req, res, next) => {
+// Endpoint for testing
+app.get('/api/actors/:actorId', async (req, res, next) => {
+  // passing values in on params
   try {
-    const { firstName, lastName } = req.body;
+    const { actorId } = req.params; // destructure actorId from params
+    if (!Number.isInteger(+actorId)) {
+      throw new ClientError(400, `Non-integer actorId: ${actorId}`);
+    }
+    const sql = `
+      select * from "actors"
+      where "actorId" = $1;
+    `;
+    const params = [actorId]; // $1
+    const result = await db.query(sql, params);
+    const actor = result.rows[0]; // get our 1 actor we got from database
+    if (!actor) throw new ClientError(404, `actor ${actorId} not found`);
+    res.json(actor); // send actor back as JSON to client
+  } catch (err) {
+    // error in query gets passed to next
+    next(err);
+  }
+});
+
+app.post('/api/actors', async (req, res, next) => {
+  // the string value is the same as a function name, arbitrary. semantically has to do w the data we are dealing with
+  try {
+    const { firstName, lastName } = req.body; // send on the body as JSON. hits middleware and gets converted to what it was before JSON
     if (!firstName || !lastName) {
       throw new ClientError(400, 'firstName and lastName are required');
     }
@@ -25,7 +49,7 @@ app.post('/api/actors', async (req, res, next) => {
     insert into "actors"("firstName", "lastName")
         values ($1, $2)
         returning *;`;
-    const params = [firstName as string, lastName as string];
+    const params = [firstName as string, lastName as string]; // firstName is $1, lastName is $2. this order comes before the insert
     const result = await db.query(sql, params);
     const newActor = result.rows[0];
     res.status(201).json(newActor);
@@ -36,13 +60,13 @@ app.post('/api/actors', async (req, res, next) => {
 
 app.put('/api/actors/:actorId', async (req, res, next) => {
   try {
-    const { firstName, lastName } = req.body;
-    if (!firstName || !lastName) {
-      throw new ClientError(400, 'firstName and lastName are required');
-    }
     const { actorId } = req.params;
     if (!Number.isInteger(+actorId)) {
       throw new ClientError(400, `Non-integer actorId: ${actorId}`);
+    }
+    const { firstName, lastName } = req.body;
+    if (!firstName || !lastName) {
+      throw new ClientError(400, 'firstName and lastName are required');
     }
     const sql = `
     update "actors"
@@ -51,7 +75,7 @@ app.put('/api/actors/:actorId', async (req, res, next) => {
       where "actorId"=$3
       returning *;`;
     const params = [firstName as string, lastName as string, actorId];
-    const result = await db.query(sql, params);
+    const result = await db.query(sql, params); // sql, with params as fill ins.
     const updatedActor = result.rows[0];
     if (!updatedActor) throw new ClientError(404, `actor ${actorId} not found`);
     res.status(200).json(updatedActor);
@@ -61,6 +85,7 @@ app.put('/api/actors/:actorId', async (req, res, next) => {
 });
 
 app.delete('/api/actors/:actorId', async (req, res, next) => {
+  // same as updates, but not as much info
   try {
     const { actorId } = req.params;
     if (!Number.isInteger(+actorId)) {
@@ -73,29 +98,8 @@ app.delete('/api/actors/:actorId', async (req, res, next) => {
     const params = [actorId];
     const result = await db.query(sql, params);
     const deletedActor = result.rows[0];
-    if (!deletedActor) throw new ClientError(404, `actor ${actorId} not found`);
-    res.status(204).json(deletedActor);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Endpoint for testing
-app.get('/api/actors/:actorId', async (req, res, next) => {
-  try {
-    const { actorId } = req.params;
-    if (!Number.isInteger(+actorId)) {
-      throw new ClientError(400, `Non-integer actorId: ${actorId}`);
-    }
-    const sql = `
-      select * from "actors"
-      where "actorId" = $1;
-    `;
-    const params = [actorId];
-    const result = await db.query(sql, params);
-    const actor = result.rows[0];
-    if (!actor) throw new ClientError(404, `actor ${actorId} not found`);
-    res.json(actor);
+    if (!deletedActor) throw new ClientError(404, `actor ${actorId} not found`); // if deletedActor is empty, the deletedActor didn't exist
+    res.sendStatus(204); // use when we aren't returning, 204 doesnt return anything, its a no content message   or can do res.status(201).json(deletedActor) to see which actor was deleted
   } catch (err) {
     next(err);
   }
