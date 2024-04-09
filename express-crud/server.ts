6a81be3428fef586236f19433f329202e004fca5
Connie Chin <conniec96@gmail.com>
2024-04-09 16:55:11 +0000

    Completed express-crud.

--- express-crud/server.ts
+++ express-crud/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import pg from 'pg';
 import { ClientError, errorMiddleware } from './lib';
+// import { Tracing } from 'trace_events';
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
@@ -28,9 +29,10 @@ app.get('/api/grades', async (req, res, next) => {
     from "grades";`;
     const result = await db.query(sql);
     const grades = result.rows;
-    res.status(200).json(grades); // might not need results attached?
+    res.status(200).json(grades);
     if (!grades) throw new ClientError(500, 'This grade query failed');
   } catch (err) {
+    // the 500
     next(err);
   }
 });
@@ -38,10 +40,9 @@ app.get('/api/grades', async (req, res, next) => {
 app.get('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const { gradeId } = req.params;
-    console.log('gradeId', gradeId);
-    // if (!Number.isInteger(+gradeId)) {
-    //   throw new ClientError(400, `Non-integer gradeId: ${gradeId}`);
-    // }
+    if (!Number.isInteger(+gradeId)) {
+      throw new ClientError(400, `Non-integer gradeId: ${gradeId}`);
+    }
     const sql = `
     select *
       from "grades"
@@ -51,6 +52,87 @@ app.get('/api/grades/:gradeId', async (req, res, next) => {
     const grade = result.rows[0];
     if (!grade) throw new ClientError(404, `grade ${gradeId} not found`);
     res.status(200).json(grade);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/api/grades', async (req, res, next) => {
+  try {
+    const { name, course, score } = req.body;
+    if (
+      !name ||
+      !course ||
+      !score ||
+      score < 0 ||
+      score > 100 ||
+      !Number.isInteger(+score)
+    ) {
+      throw new ClientError(400, `Invalid input for grade`);
+    }
+    const sql = `
+    insert into "grades" ("name", "course", "score")
+                values ($1, $2, $3)
+                returning *;
+    `;
+    const params = [name as string, course as string, score as number];
+    const result = await db.query(sql, params);
+    const insertedGrade = result.rows[0];
+    res.status(201).json(insertedGrade);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/api/grades/:gradeId', async (req, res, next) => {
+  try {
+    const { gradeId } = req.params;
+    if (!Number.isInteger(+gradeId)) {
+      throw new ClientError(400, `Non-integer gradeId: ${gradeId}`);
+    }
+    const { name, course, score } = req.body;
+    if (
+      !name ||
+      !course ||
+      !score ||
+      score < 0 ||
+      score > 100 ||
+      !Number.isInteger(+score)
+    ) {
+      throw new ClientError(400, `Invalid input for grade`);
+    }
+    const sql = `
+    update "grades"
+    set "name" = $1,
+        "course" = $2,
+        "score" = $3
+    where "gradeId" = $4
+    returning *;`;
+    const params = [name as string, course as string, score as number, gradeId];
+    const result = await db.query(sql, params);
+    const updatedGrade = result.rows[0];
+    if (!updatedGrade) throw new ClientError(404, `grade ${gradeId} not found`);
+    res.status(200).json(updatedGrade);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.delete('/api/grades/:gradeId', async (req, res, next) => {
+  try {
+    const { gradeId } = req.params;
+    if (!Number.isInteger(+gradeId)) {
+      throw new ClientError(400, `Non-integer gradeId: ${gradeId}`);
+    }
+    const sql = `
+    delete from "grades"
+          where "gradeId"=$1
+          returning *;`;
+    const params = [gradeId];
+    const result = await db.query(sql, params);
+    const deletedGrade = result.rows[0];
+    if (!deletedGrade) throw new ClientError(404, `actor ${gradeId} not found`);
+    res.sendStatus(204);
   } catch (err) {
     next(err);
   }
